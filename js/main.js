21700ef73a6bd18327bf1a6a0f1c82a06f9a64e7
Paco Fuentes <pacofuentes.work@gmail.com>
2023-09-21 21:11:01 +0200

    Primer intento de cambiar imagen del div con un solo boton

--- js/main.js
+++ js/main.js
@@ -4,3 +4,61 @@ console.log("щ（ﾟДﾟщ） < “Dear god why‽ "); // Funciona consola
 console.log("Descomenta para comprobar la consola");
 console.log("----------------------------------------------------------------");
 console.log("----------------------------------------------------------------");
+
+
+/* ------------ Código David ---------------- */
+
+let pantalla = document.getElementById("screen");
+
+
+
+document.addEventListener("click", (e) => {
+    
+    let lastState;
+    let stateOn;
+
+    if (e.target.id === 'botA') {
+
+        // pantalla.style.backgroundColor = "red";
+        pantalla.style.backgroundImage = "url('img/kawai.jpg')";
+
+    } else if (e.target.id === 'botB') {
+
+        // pantalla.style.backgroundColor = "grey";
+        pantalla.style.backgroundImage = "url('img/bear.jpg')";
+
+    } else if (e.target.id === 'start') {
+
+        // pantalla.style.backgroundColor = "grey";
+        pantalla.style.backgroundImage = "url('img/original.jpg')";
+
+    } else if (stateOn == false && e.target.id === 'onOff') {
+
+        lastState = false;
+        stateOn = true;
+
+        // pantalla.style.backgroundColor = "grey";
+        pantalla.style.backgroundImage = "url('img/images.jpeg')";
+
+    } else if (lastState == false && e.target.id === 'onOff') {
+
+        lastState = true
+        stateOn = false;
+
+        // pantalla.style.backgroundColor = "grey";
+        pantalla.style.backgroundImage = "url('img/10193.png')";
+
+    };
+
+    console.log(stateOn);
+    console.log(lastState);
+    
+
+});
+
+
+
+/* ------------------------------------------- */
+
+
+/////QUIZÁ SEA POSIBLE HACER UN SWITCH PAR ACAMBIAR DE ESTADO Y USARLO PARA EL ON / OFF
