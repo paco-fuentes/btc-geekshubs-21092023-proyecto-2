77fd1c738f701699a7f97afad9dd9961d2056052
Paco Fuentes <pacofuentes.work@gmail.com>
2023-09-24 21:49:56 +0200

    Logica de los botones de la GAMEBOY con javascript funciona correctamente

--- js/main.js
+++ js/main.js
@@ -6,14 +6,11 @@ console.log("----------------------------------------------------------------");
 console.log("----------------------------------------------------------------");
 
 
-/* ------------ Código David Mod ---------------- */
-
-
 let pantalla = document.getElementById("actionScreen");
 
 
-let lastState = true;
-let stateOn = false;
+let lastState = false;
+let stateOn = true;
 
 document.addEventListener("click", (e) => {
 
@@ -21,46 +18,59 @@ document.addEventListener("click", (e) => {
         stateOn = lastState;
     }
 
-    if (e.target.id === 'actionA') {
 
-        // pantalla.style.backgroundColor = "red";
-        pantalla.style.backgroundImage = "url('../img/a-1.png')";
 
-    } else if (e.target.id === 'actionB') {
+    if (stateOn == true && e.target.id === 'actionOnOff') {
 
-        // pantalla.style.backgroundColor = "grey";
-        pantalla.style.backgroundImage = "url('../img/b-1.png')";
-
-    } else if (e.target.id === 'actionStart') {
-
-        // pantalla.style.backgroundColor = "grey";
-        pantalla.style.backgroundImage = "url('../img/start-1.png')";
-
-    } else if (e.target.id === 'actionSelect') {
-
-        // pantalla.style.backgroundColor = "grey";
-        pantalla.style.backgroundImage = "url('../img/select-1.png')";
-
-    } else if (stateOn == false && e.target.id === 'actionOnOff') {
-
-        /* stateOn = true; */
-        lastState = true;
-
-        // pantalla.style.backgroundColor = "grey";
-        pantalla.style.backgroundImage = "url('../img/on-1.png')";
-
-    } else if (stateOn == true && e.target.id === 'actionOnOff') {
-
+        
         lastState = false;
-        /*  stateOn = false; */
 
-        // pantalla.style.backgroundColor = "grey";
-        pantalla.style.backgroundImage = "url('../img/off-1.png')";
+        ledTop.style.backgroundColor = "black";
+        pantalla.style.backgroundImage = "url('../img/')";
 
     };
 
-    console.log(stateOn);
-    console.log(lastState);
+        if (stateOn == true && e.target.id === 'actionA') {
+
+            // pantalla.style.backgroundColor = "red";
+            pantalla.style.backgroundImage = "url('../img/a-1.png')";
+    
+        } 
+        
+        if (stateOn == true && e.target.id === 'actionB') {
+    
+            // pantalla.style.backgroundColor = "grey";
+            pantalla.style.backgroundImage = "url('../img/b-1.png')";
+    
+        } 
+        
+        if (stateOn == true && e.target.id === 'actionStart') {
+    
+            // pantalla.style.backgroundColor = "grey";
+            pantalla.style.backgroundImage = "url('../img/start-1.png')";
+    
+        } 
+        
+        if (stateOn == true && e.target.id === 'actionSelect') {
+    
+            // pantalla.style.backgroundColor = "grey";
+            pantalla.style.backgroundImage = "url('../img/select-1.png')";
+    
+        } 
+
+    
+
+    if (stateOn == false && e.target.id === 'actionOnOff') {
+
+        lastState = true;
+
+        ledTop.style.backgroundColor = "red";
+        pantalla.style.backgroundImage = "url('../img/on-1.png')";
+
+    };
+
+    console.log(`Estado anteior: ${stateOn}`);
+    console.log(`Estado actual: ${lastState}`);
 
 
 });
