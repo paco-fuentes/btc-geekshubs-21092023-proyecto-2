f7007b6c052ed21099d84f84dff5c661c5c7f07e
Paco Fuentes <pacofuentes.work@gmail.com>
2023-09-25 19:18:48 +0200

    Editadas imagenes con transparencia de fondo. Añadida carpeta assets para intentar poner un sonido de encendido

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ document.addEventListener("click", (e) => {
 
 
     if (stateOn == true && e.target.id === 'actionOnOff') {
-        
+
         lastState = false;
 
         ledTop.style.backgroundColor = "black";
@@ -28,43 +28,68 @@ document.addEventListener("click", (e) => {
 
     };
 
-        if (stateOn == true && e.target.id === 'actionA') {
+    if (stateOn == true && e.target.id === 'actionTop') {
 
-            pantalla.style.backgroundImage = "url('./img/a-1.png')";
+        pantalla.style.backgroundImage = "url('./img/top.png')";
 
-        } 
-        
-        if (stateOn == true && e.target.id === 'actionB') {
-    
-            pantalla.style.backgroundImage = "url('./img/b-1.png')";
-    
-        } 
-        
-        if (stateOn == true && e.target.id === 'actionStart') {
-    
-            pantalla.style.backgroundImage = "url('./img/select-1.png')";
-    
-        } 
-        
-        if (stateOn == true && e.target.id === 'actionSelect') {
-    
-            pantalla.style.backgroundImage = "url('./img/start-1.png')";
-    
-        } 
+    }
 
-    
+
+    if (stateOn == true && e.target.id === 'actionBottom') {
+
+        pantalla.style.backgroundImage = "url('./img/bottom.png')";
+
+    }
+
+    if (stateOn == true && e.target.id === 'actionRight') {
+
+        pantalla.style.backgroundImage = "url('./img/right.png')";
+
+    }
+
+    if (stateOn == true && e.target.id === 'actionLeft') {
+
+        pantalla.style.backgroundImage = "url('./img/left.png')";
+
+    }
+
+    if (stateOn == true && e.target.id === 'actionA') {
+
+        pantalla.style.backgroundImage = "url('./img/a-1-trans.png')";
+
+    }
+
+    if (stateOn == true && e.target.id === 'actionB') {
+
+        pantalla.style.backgroundImage = "url('./img/b-1-trans.png')";
+
+    }
+
+    if (stateOn == true && e.target.id === 'actionStart') {
+
+        pantalla.style.backgroundImage = "url('./img/select-1-trans.png')";
+
+    }
+
+    if (stateOn == true && e.target.id === 'actionSelect') {
+
+        pantalla.style.backgroundImage = "url('./img/start-1-trans.png')";
+
+    }
+
+
 
     if (stateOn == false && e.target.id === 'actionOnOff') {
 
         lastState = true;
 
         ledTop.style.backgroundColor = "red";
-        pantalla.style.backgroundImage = "url('./img/on-1.png')";
+        pantalla.style.backgroundImage = "url('./img/on-1-trans.png')";
 
     };
 
     console.log(`Estado anteior: ${stateOn}`);
-    console.log(`Estado actual: ${lastState}`);
+    console.log(`Estado actual: ${lastState}`); /* he nombrado las variables al revés -------- ª{•̃̾_•̃̾}ª */
 
 
 });
